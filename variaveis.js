e0ea74477b304c75ca6d8267ddc2ee537c36ac7f
gabrielanori <gabriela.nori1@gmail.com>
2020-12-07 18:38:22 -0300

    third commit

---
+++ variaveis.js
@@ -0,0 +1,24 @@
+console.log("=== VARIAVEIS ===")
+
+// Comentários única linha
+
+/* 
+Comentário
+Multiplas 
+Linhas
+*/
+
+/* */
+
+// Camada de Armazenamento
+// var chave = "valor";
+//let chave = "valor";
+//const chave = "valor";
+//var e let são equivalentes (mas tem diferenças)
+const nome = "Leonardo"; // var - ES5
+let idade = 37; // let - ES6
+let peso = 100; // const - ES6
+
+console.log("nome:", nome);
+console.log("idade", idade);
+console.log("peso:", peso);
