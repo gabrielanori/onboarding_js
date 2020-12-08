6e298201f61f9421258b700a1ca33e2c75f4ca2e
gabrielanori <gabriela.nori1@gmail.com>
2020-12-08 11:48:43 -0300

    fourth commit

--- variaveis.js
+++ variaveis.js
@@ -15,10 +15,12 @@ Linhas
 //let chave = "valor";
 //const chave = "valor";
 //var e let são equivalentes (mas tem diferenças)
-const nome = "Leonardo"; // var - ES5
+const nome = "Leonardo"; // const - ES5
 let idade = 37; // let - ES6
-let peso = 100; // const - ES6
+let altura = 1.89; // let ES6
+var magro = false; // var - ES6
 
-console.log("nome:", nome);
-console.log("idade", idade);
-console.log("peso:", peso);
+console.log("nome:", nome, typeof nome); //typeof mostra o tipo da variável
+console.log("idade", idade, typeof idade);
+console.log("altura", altura, typeof altura);
+console.log("magro", magro, typeof magro);
