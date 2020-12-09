2e5bcb9ac57f0d27eb08039cc8479851a4e23033
gabrielanori <gabriela.nori1@gmail.com>
2020-12-09 17:41:25 -0300

    sixth commit

--- instrucoes.js
+++ instrucoes.js
@@ -1,2 +1,11 @@
 console.log("")
 console.log("=== INSTRUCOES ===")
+
+// Camada de Armazenamento
+// var chave = valor;
+
+//Camada de Instruções (Comando)
+//comando();
+//print(); 
+
+// let age = prompt("Digite sua Idade", "Sua Idade Aqui"); // se eu colocasse soh age ele iria como var seria uma variável implícita
--- paradigmas.js
+++ paradigmas.js
@@ -1,2 +1,26 @@
 console.log("")
 console.log("=== PARADIGMAS ===")
+
+//paradigmas formatos padrões que posso adotar nos codigos
+
+// Sintaxe Literal
+let escola = "Caelum";
+let tempo = 15;
+
+
+
+
+// Sintaxe OOP
+let school = new String("Caelum");
+let time = new Number(15);
+
+
+
+console.log("escola", escola, typeof escola, escola.constructor);
+console.log("school", school, typeof school, school.constructor);
+
+console.dir(String.prototype); // mostra no console, la na pag todos os comandos que conseguem manipular strings
+// API recurso disponível ... comandos dentro da estrutura da linguagem, neste caso recursos disponíveis para mexer com o texto
+
+console.log("tempo", tempo, typeof tempo);
+console.log("time", time, typeof time);
