1efc7aae914396b7e3bd4ed3d73c99fde8e66dfe
gabrielanori <gabriela.nori1@gmail.com>
2020-12-11 19:20:02 -0300

    twelve commit

--- objetos.js
+++ objetos.js
@@ -19,8 +19,6 @@ console.log("=== OBJETOS ===");
     modelo
     fabric
 
-
-
                  PESSOA
                /        \
   CARACTERISTICAS       AÇÕES
@@ -31,10 +29,6 @@ console.log("=== OBJETOS ===");
     idade             dormir()
     nome               
     etinia
-    
-
-
-
 */ 
 
 // Sintaxe Literal
@@ -42,8 +36,38 @@ const carro = {}; // quando mandarmos logar alguma coisa la no console da pagina
 // JavaScript permite que vc crie objetos de 2 formas: voce pode começar com objeto vazio e ir linha a linha construindo montando incrementando seu obj (uma abordagem sintatica que o JS tem) e tem varios casos de uso no dia a dia
 
 //Características ==> Propriedades
-// dot notation 
-// objeto.propriedade = valor; é uma caracteristica q usamos pra ir criando e adicionando caracteírticas aos seus objetos
 
+// Dot Notation 
+// objeto.propriedade = valor; é uma caracteristica q usamos pra ir criando e adicionando caracteírticas aos seus objetos
+carro.modelo = "Uno"; // acrescentei esta propriedade dentro do objeto {modelo: "Uno"}
+carro.ano = 2020;
+carro.economico = true;
+
+
+// Bracket Notation
+// objeto["propriedade"] = valor;
+carro["cor"] = "amarelo";
+carro["potencia"] = 1.6;
+carro["ar"] = false
+
+
+// Ações ==> Métodos
+// instruções funções comandos
+//Dot Notation
+//objeto.metodo = function() {}
+carro.ligar = function() {
+  return "ON";
+}
+
+carro.desligar = function() {
+  return "OFF";
+}
+
+//Bracket Notation
+// objeto["metodo"] = function() {}
+carro["buzinar"] = function() {
+return "Fom FOM"
+
+}
 
 console.log(carro);
