349eecef802169004e08088081185a814a40765d
gabrielanori <gabriela.nori1@gmail.com>
2020-12-10 21:54:10 -0300

    eigth commit

---
+++ array.js
@@ -0,0 +1,17 @@
+console.log("=== ARRAY ===");
+
+console.dir(Array.prototype); // todos os comandos que fazem parte da API de Array caixa de ferramentas que temos a disposicao para mexer com o q esta em baixo, em JavaScript
+
+//Sintaxe Literal
+let carros = ["HB20", "Toro", "Corsa", "Celta"];
+
+console.log(carros);
+//console.log(carros.constructor);
+
+//Sintaxe OOp
+let cars = new Array("Gol", "Palio", "Uno");
+
+console.log(cars);
+//console.log(cars.constructor);
+
+//length dimensão do array
--- funcoes.js
+++ funcoes.js
@@ -1,12 +1,18 @@
 console.log("=== FUNCOES ===");
 
 // Funções Nomeadas (criar uma função é criar um comando, criar uma instrução e adicionar na minha página pq a instrução vai resolver algum problema)
-
 function soma (n1, n2) { // executa essa instrução
     let resultado = n1 + n2; //Processamento //implementação de uma função 
     //console.log(resultado);
     return resultado; // resposta do processamento
 }
 
+console.log( soma(4, 8) ); //para pegar o resultado dessa informação preciso do console.log
 
 // Funções Anônimas
+let sum = function (n1, n2) { // associa a esta variável, atribui a esta variável a estrutura da função anonima
+ let result = n1 + n2;
+ return result;
+}
+
+console.log( sum(8, 7) );
