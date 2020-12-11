97113dac42394c2772d6183869562fa9bdd99c84
gabrielanori <gabriela.nori1@gmail.com>
2020-12-11 14:23:27 -0300

    tenth commit

---
+++ objeto.js
@@ -0,0 +1,38 @@
+console.log("=== OBJETOS ===");
+
+/*
+                 OBJETO
+               /        \
+  CARACTERISTICAS       AÇÕES
+    como ele é       o q é capaz de fazer     
+
+
+                 CARRO
+               /        \
+  CARACTERISTICAS       AÇÕES
+    batido              ligar()
+    econ                desligar()
+    ar                  acelrar()
+    cor                 freiar()
+    pot                 buzinar()
+    ano                 
+    modelo
+    fabric
+
+
+
+                 PESSOA
+               /        \
+  CARACTERISTICAS       AÇÕES
+    peso              andar()
+    altura            beber()
+    cor cabelo        falar()
+    cor pele          comer()
+    idade             dormir()
+    nome               
+    etinia
+    
+
+
+
+*/ 
