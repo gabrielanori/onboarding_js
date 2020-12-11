ff0bc7fc7b22e19459fb4ab1549cd4985188a811
gabrielanori <gabriela.nori1@gmail.com>
2020-12-11 14:39:51 -0300

    eleven commit

--- objetos.js
+++ objetos.js
@@ -36,3 +36,14 @@ console.log("=== OBJETOS ===");
 
 
 */ 
+
+// Sintaxe Literal
+const carro = {}; // quando mandarmos logar alguma coisa la no console da pagina e ele aparecer assim {} é pq estou trabalhando com um objeto, neste exemplo o objeto esta vazio
+// JavaScript permite que vc crie objetos de 2 formas: voce pode começar com objeto vazio e ir linha a linha construindo montando incrementando seu obj (uma abordagem sintatica que o JS tem) e tem varios casos de uso no dia a dia
+
+//Características ==> Propriedades
+// dot notation 
+// objeto.propriedade = valor; é uma caracteristica q usamos pra ir criando e adicionando caracteírticas aos seus objetos
+
+
+console.log(carro);
