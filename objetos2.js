b4d0aaaa6227821db2628428683ce837670e99b7
gabrielanori <gabriela.nori1@gmail.com>
2020-12-16 10:40:21 -0300

    14 commit

--- objetos2.js
+++ objetos2.js
@@ -16,12 +16,19 @@ console.log("=== OBJETOS - PARTE 2 ===");
 
 // Sintaxe Literal
 const auto = {
+    status: false,
     modelo: "Fox",
     ano: 2019,
     economico: true,
     cor: "vermelho",
-    potencia: 1.0
-
+    potencia: 1.6,
+    ligar: function() {
+        return this.status = true; //quando usamos o THIS dentro do método e esse método esta atrelado a um obj, se refere dentro do contexto trabalhado, nesse caso eh o auto, representação do obj no contexto que estamos trabalhando
+    },
+    desligar: function() {
+        return auto.status = false;
+    }
+ 
 }; // cria um obj equivalente ao: const auto = new Object();
 // todo objeto tem um protótipo,da onde ele nasceu, nasce a partir de algum construtor _proto_: Object
 
---
+++ regex.js
@@ -0,0 +1,52 @@
+console.log("=== REGEX ===");
+
+/* 
+
+
+
+METACARACTERES
+
+^INCIO DA DA LINHA
+$ FIM DA LINHA
+
+[i-f] LISTA PERMITIDA (INTERVALO) ESSE É UM EXEMPLO PODERIA SER [A-Z]
+{min,max} QUANTIFICADOR
+\c ESCAPE (CARACTERIES ESPECIAIS)
+? OPCIONAL
+
+\d APENAS DIGITOS
+\w CONJUNTO ALFANUMERICO
+() AGRUPADOR
+
+*/
+
+// CEP 1: ^[0-9]{5}\-?[0-9]{3}$
+// CEP 2: ^\d{5}\-?\d{3}$
+//EMAIL:  ^[A-z0-9\.\-\_]{1,}\.\w{1,}(\.\w{2})?$
+//PLACA DE CARRO: ^[A-z]{3}\-\d{4}$
+
+let meuCep = prompt("Digite seu CEP?");
+console.log(meuCep, typeof meuCep);
+
+//Regex - Sintaxe Literal
+let regexCep = /^\d{5}\-?\d{3}$/;
+console.log(regexCep);
+
+// objetoRegex.test(string)
+// objetoRegex.exec(string)
+
+
+console.log(
+    regexCep.test(meuCep) // esse mostra se eh true ou false o q escrevi cep no prompt
+
+);
+
+console.log(
+    regexCep.exec(meuCep) // esse mostra se eh true ou false o q escrevi cep no prompt
+
+);
+
+console.log(
+    regexCep.match(regexCep) // volta null match ou exec qdo nao há correspondencia na expressão ou no valor de entrada
+
+);
